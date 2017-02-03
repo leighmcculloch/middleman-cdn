26e4995e7b6f3d85f11f59203b431475d07e83de
Manuel Meurer <manuel@krautcomputing.com>
2017-02-03 10:02:36 +0100

    middleman 4 compat

--- lib/middleman-cdn.rb
+++ lib/middleman-cdn.rb
@@ -2,4 +2,4 @@ require 'middleman-core'
 require 'middleman-cdn/commands'
 require "middleman-cdn/extension"
 
-::Middleman::Extensions.register(:cdn, ::Middleman::CDN::CDNExtension)
+::Middleman::Extensions.register(:cdn, ::Middleman::CDN::Extension)
--- lib/middleman-cdn/extension.rb
+++ lib/middleman-cdn/extension.rb
@@ -2,17 +2,7 @@ require 'middleman-core'
 
 module Middleman
   module CDN
-    module Helpers
-      def cdn_invalidate(files = nil)
-        ::Middleman::Cli::CDN.new.cdn_invalidate(cdn_options, *files)
-      end
-
-      def cdn_options
-        ::Middleman::CDN::CDNExtension.options
-      end
-    end
-
-    class CDNExtension < Middleman::Extension
+    class Extension < Middleman::Extension
       option :cloudflare, nil, 'CloudFlare options'
       option :cloudfront, nil, 'CloudFront options'
       option :fastly, nil, 'Fastly options'
@@ -21,28 +11,31 @@ module Middleman
       option :filter, nil, 'Cloudflare options'
       option :after_build, false, 'Cloudflare options'
 
+      @@cdn_options = nil
+
       def initialize(app, options_hash = {}, &block)
         super
 
         @@cdn_options = options
-
-        app.after_configuration do
-          app.after_build do
-            cdn_invalidate if cdn_options.after_build
-          end
-        end
-
-        app.send :include, Helpers
-      end
-
-      def registered
-        included
       end
 
       def self.options
         @@cdn_options
       end
 
+      def after_build(builder)
+        ::Middleman::Cli::CDN.new.cdn_invalidate(options) if options.after_build
+      end
+
+      helpers do
+        def cdn_invalidate(files = nil)
+          ::Middleman::Cli::CDN.new.cdn_invalidate(cdn_options, *files)
+        end
+
+        def cdn_options
+          ::Middleman::CDN::Extension.options
+        end
+      end
     end
   end
 end
